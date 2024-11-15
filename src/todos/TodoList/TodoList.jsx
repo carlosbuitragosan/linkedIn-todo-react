990e2733d094c345cb12e185b042f4358808301b
Carlos Buitrago <letscatchupagain@gmail.com>
2024-11-15 15:06:01 +0000

    Remove RemoveTodo prop from TodoList and add directly in TodoListItem.

--- src/todos/TodoList/TodoList.jsx
+++ src/todos/TodoList/TodoList.jsx
@@ -1,22 +1,16 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import TodoListItem from "../TodoListItem/TodoListItem";
 import NewTodoForm from "../NewTodoForm/NewTodoForm";
-import { removeTodo } from "../../redux/todosSlice";
 
 function TodoList() {
   const todos = useSelector((state) => state.todos);
-  const dispatch = useDispatch();
 
   return (
     <div className="list-wrapper">
       <NewTodoForm />
       {todos.map((todo) => (
-        <TodoListItem
-          todo={todo}
-          key={todo.text}
-          removeTodo={() => dispatch(removeTodo({ text: todo.text }))}
-        />
+        <TodoListItem todoText={todo} key={todo.text} />
       ))}
     </div>
   );
--- src/todos/TodoListItem/TodoListItem.jsx
+++ src/todos/TodoListItem/TodoListItem.jsx
@@ -1,6 +1,10 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { removeTodo } from "../../redux/todosSlice";
 
-function TodoListItem({ todo, removeTodo }) {
+function TodoListItem({ todo }) {
+  const dispatch = useDispatch();
+
   return (
     <div className="todo-item-container">
       <h3>{todo.text}</h3>
@@ -8,7 +12,11 @@ function TodoListItem({ todo, removeTodo }) {
         <button type="button" className="completed-button">
           Mark as Completed
         </button>
-        <button type="button" className="remove-button" onClick={removeTodo}>
+        <button
+          type="button"
+          className="remove-button"
+          onClick={() => dispatch(removeTodo({ text: todo.text }))}
+        >
           Remove
         </button>
       </div>
